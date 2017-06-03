a30c5cb64ab3a4555b62bcbbf2cbb721fb9a19fd
innlouvate <l.j.franklin85@googlemail.com>
2017-06-03 19:34:21 +0100

    multi-speed falling

--- src/snow/snow.clj
+++ src/snow/snow.clj
@@ -2,22 +2,28 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(def x-params [10 200 390])
+
 (defn setup []
   (q/smooth)
   {:flake (q/load-image "images/white_flake.png")
    :background (q/load-image "images/blue_background.png")
-   :y-param 10})
+   :y-params [{:y 10 :speed 1} {:y 150 :speed 4} {:y 50 :speed 2}]})
 
-(defn draw [{flake :flake background :background y-param :y-param}]
-  (q/background-image background)
-  (def x-params [10 200 390])
-  (doseq [x x-params]
-    (q/image flake x y-param)))
+(defn update-y
+  [{y :y speed :speed :as m}]
+  (if (>= y (q/height))
+    (assoc m :y 0)
+    (update-in m [:y] + speed)))
 
 (defn update [state]
-  (if (>= (:y-param state) (q/height))
-    (assoc state :y-param 0)
-    (update-in state [:y-param] inc)))
+  (let [y-params (:y-params state)]
+    (assoc state :y-params (map update-y y-params))))
+
+(defn draw [{flake :flake background :background y-params :y-params}]
+  (q/background-image background)
+  (dotimes [n 3]
+    (q/image flake (nth x-params n) (:y (nth y-params n)))))
 
 (q/defsketch practice
   :title "Snow scene"
